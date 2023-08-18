054c7f6c1b714c3f1bd10b6a36e7f9c9e25f16bc
Seth Mwebi <sethmwebi27@gmail.com>
2023-08-18 09:50:21 +0300

    Answer Flatlist

--- app/[id].js
+++ app/[id].js
@@ -1,21 +1,32 @@
-import { View, Text } from 'react-native'
-import { useSearchParams } from "expo-router"
-import questions from "../data/questions"
-import QuestionHeader from "../src/components/QuestionHeader"
+import { View, Text, FlatList } from "react-native";
+import { useSearchParams } from "expo-router";
+import questions from "../data/questions";
+import answers from "../data/answers";
+import QuestionHeader from "../src/components/QuestionHeader";
+import AnswerListItem from "../src/components/AnswerListItem";
 
 const QuestionDetailsPage = () => {
-	const { id } = useSearchParams()
+	const { id } = useSearchParams();
 
-	const question = questions.items.find(q => q.question_id == id)
+	const question = questions.items.find((q) => q.question_id == id);
 
-	if(!question){
-		return <Text>Question not found!</Text>
+	if (!question) {
+		return <Text>Question not found!</Text>;
 	}
+	// return (
+	// 	<View style={{backgroundColor: "white", flex: 1}}>
+	// 		<QuestionHeader question={question}/>
+	// 	</View>
+	// )
 	return (
-		<View style={{backgroundColor: "white", flex: 1}}>
-			<QuestionHeader question={question}/>
+		<View style={{ backgroundColor: "white", flex: 1 }}>
+			<FlatList
+				data={answers.items}
+				renderItem={({ item }) => <AnswerListItem answer={item} />}
+				ListHeaderComponent={() => <QuestionHeader question={question}/>}
+			/>
 		</View>
-	)
-}
+	);
+};
 
-export default QuestionDetailsPage
+export default QuestionDetailsPage;
